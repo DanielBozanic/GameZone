527602047d24cd3e902373f2c3e83ced654131d8
DanielBozanic <68564905+DanielBozanic@users.noreply.github.com>
2022-07-05 19:27:24 +0200

    React library versions changed

--- gamezone-frontend/src/Layout/Footer.js
+++ gamezone-frontend/src/Layout/Footer.js
@@ -1,12 +1,15 @@
 import logo from "../Assets/images/logo.PNG";
 import "../Assets/css/footer.css";
+import { Navbar, NavbarBrand } from "reactstrap";
 
 const Footer = () => {
-  return (
-    <div className="footer">
-      <img src={logo} alt="Logo" className="responsive-img-footer" />
-    </div>
-  );
+	return (
+		<Navbar className="footer" color="dark" expand="md" dark>
+			<NavbarBrand>
+				<img src={logo} alt="Logo" className="responsive-img-footer" />
+			</NavbarBrand>
+		</Navbar>
+	);
 };
 
 export default Footer;
--- gamezone-frontend/src/Layout/Header.js
+++ gamezone-frontend/src/Layout/Header.js
@@ -1,19 +1,15 @@
 import logo from "../Assets/images/logo.PNG";
 import "../Assets/css/header.css";
-import { Row, Col, Container } from "reactstrap";
+import { Navbar, NavbarBrand } from "reactstrap";
 
 const Header = () => {
-  return (
-    <div className="header">
-      <Container>
-        <Row>
-          <Col>
-            <img src={logo} alt="Logo" className="responsive-img-header" />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+	return (
+		<Navbar className="header" color="dark" expand="md" dark>
+			<NavbarBrand>
+				<img src={logo} alt="Logo" className="responsive-img-header" />
+			</NavbarBrand>
+		</Navbar>
+	);
 };
 
 export default Header;
