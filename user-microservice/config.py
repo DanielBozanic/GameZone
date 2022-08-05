27e5b21b802cc2313822051c860279cbc935bdb2
DanielBozanic <68564905+DanielBozanic@users.noreply.github.com>
2022-08-05 23:09:26 +0200

    Started business report microservice

--- user-microservice/config.py
+++ user-microservice/config.py
@@ -6,6 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/users-db?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://testuser:PassWord_FOR_NTP!@localhost:3306/users-db?charset=utf8mb4'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
