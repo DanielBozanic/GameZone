c11c72bba9023bd281b0334ecb12661bba771093
DanielBozanic <68564905+DanielBozanic@users.noreply.github.com>
2022-08-17 22:51:54 +0200

    More db fixes

--- database-files/databases-setup.sql
+++ database-files/databases-setup.sql
@@ -4,5 +4,6 @@ CREATE DATABASE IF NOT EXISTS `contact-and-report-db`;
 CREATE DATABASE IF NOT EXISTS `news-db`;
 CREATE DATABASE IF NOT EXISTS `users-db`;
 
-CREATE USER IF NOT EXISTS 'testuser'@'localhost' IDENTIFIED BY 'PassWord_FOR_NTP!';
-GRANT ALL ON *.*  TO 'testuser'@'localhost';
+CREATE USER IF NOT EXISTS 'testuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'PassWord_FOR_NTP!';
+GRANT ALL PRIVILEGES ON *.*  TO 'testuser'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
